a274843c01e86e9817b1a8c7affa989bb822bae9
Chonyyy <mlchony7@gmail.com>
2024-07-17 06:59:30 -0600

    moving discovery to node

--- distribufy/app.py
+++ distribufy/app.py
@@ -20,22 +20,10 @@ if __name__ == "__main__":
         filemode= 'w')
     
     logger.setLevel(logging.DEBUG)
-
-    discovered_ip = None
-    multicast_thread = threading.Thread(target=send_multicast, daemon=True)
-    multicast_thread.start()#FIXME: Discovery not always works
     database_name = f'db_{ip}'
     
-    discovered_ip = receive_multicast()
-    if discovered_ip:
-        if len(sys.argv) >= 2:
-            role = str(sys.argv[1])
-            start_server(ip, discovered_ip, role, db_name=database_name)
-        else:
-            start_server(ip, discovered_ip, db_name=database_name)
+    if len(sys.argv) >= 2:
+        role = str(sys.argv[1])
+        start_server(ip, role, db_name=database_name)
     else:
-        if len(sys.argv) >= 2:
-            role = str(sys.argv[1])
-            start_server(ip, role, db_name=database_name)
-        else:
-            start_server(ip, db_name=database_name)
+        start_server(ip, db_name=database_name)
