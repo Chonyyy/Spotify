572a691c226113bf3faa797a10c909ba7ef3c948
Alejandro Yero Valdes <alejandroyeroval@gmail.com>
2024-10-11 00:33:36 -0400

    more gateway stuff

--- distribufy/services/gateway/gateway_reference.py
+++ distribufy/services/gateway/gateway_reference.py
@@ -14,33 +14,19 @@ logger_le = logging.getLogger("__main__.le")
 logger_dt = logging.getLogger("__main__.dt")
 
 class GatewayReference(ChordNodeReference):
+    pass
     
-    def new_leader(self, leader: 'GatewayReference'):
-        """Notify the node of a leader change."""
-        self._send_request('/new_leader', {'id': leader.id, 'ip': leader.ip})
+    # def notify(self, node):
+    #     """Notify the node of a leader change."""
+    #     self._send_request('/gw/notify', {'id': node.id, 'ip': node.ip})
     
-    def share_knowledge(self, known_nodes: List['GatewayReference']):
-        """Notify the new leader."""
-        nodes = []
-        for node in known_nodes:
-            nodes.append({'ip': node.ip, 'port': node.port, 'id': node.id})
-        self._send_request('/share_knowledge', {'nodes': nodes})
-    
-    def ping(self):
-        self._send_request('/ping', method='get')
-        
-    def update_data(self, data):
-        """Update the node's data store with the provided data."""
-        try:
-            payload = {
-                'gateway': [],
-                'ftp': [],
-                'music-manager': []
-            }
-            for key in payload.keys():
-                for reference in data[key]:
-                    payload[key].append({'ip':reference.ip, 'port':reference.port})
-            self._send_request('/rep_data', {'data': payload})
-            logger.info(f"Data replication updated with new data: {payload}")
-        except Exception as e:
-            logger.error(f'while replicating: {e}')
+    # def share_gw_knowledge(self, known_nodes: List['GatewayReference']):
+    #     """Notify the new leader."""
+    #     nodes = []
+    #     for node in known_nodes:
+    #         nodes.append({'ip': node.ip, 'port': node.port, 'id': node.id})
+    #     self._send_request('/gw/share-gw-knowledge', {'nodes': nodes})
+
+    # @property
+    # def gateway_nodes(self):
+    #     return self._send_request('/gw/gateway-nodes', method='get')
